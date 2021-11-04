dc25709dcda03b6ba195ee89c04f92d787b88671
denis <okposhka09@gmail.com>
2021-11-04 17:46:31 +0700

    Add EF

---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext
+    {
+    }
+}
